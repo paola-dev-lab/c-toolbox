06647554f3e5c9fd42545ef0eaf6b92bc40d3cd2
Paola P <dppuertol@gmail.com>
2026-03-06 19:26:13 +0000

    main test put into comments

--- 01-src/my_strlen.c
+++ 01-src/my_strlen.c
@@ -12,10 +12,10 @@ int my_strlen(char *s)
     return (i);
 }
 
-int main(int argc, char **argv)
+/*int main(int argc, char **argv)
 {
     if (argc != 2)
         return (0);
     printf("%d\n", my_strlen(argv[1]));
     return (0);
-}
+}*/
