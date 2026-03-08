b35b73c350ff62dceed01adab5bc3bd9d764f12d
Paola P <dppuertol@gmail.com>
2026-03-08 11:03:07 +0000

    code helper : find_start added

--- 01-src/csv_get_field.c
+++ 01-src/csv_get_field.c
@@ -27,12 +27,60 @@ int field_len(char *line, char sep, int field_index)
     return (-1);
 }
 
+int find_start(char *line, char sep, int field_index)
+{
+     int start;
+     int i;
+     int f;
+
+     i = 0;
+     f = 0;
+     start = -1;
+     if (field_index == 0)
+        start = 0;
+    else 
+    {
+        while (line[i])
+        {
+            if(line[i] == sep)
+                f++;
+            if (f == field_index)
+            {
+                start = i + 1;
+                    break;
+            }
+        i++;
+        }
+    }
+    return (start);
+}
+
+char *my_subs *str, int start, int len)
+{
+   
+     char *newf;
+     int i;
+     *newf = malloc(sizeof(char) + (len + 1));
+        if (!newf)
+            return NULL;
+        i = 0;
+        while(
+}
+
+
 char *csv_get_field(const char *line, char sep, int field_index)
 {
     int i;
+    int f;
     int flen;
+   
     if (line == NULL || field_index < 0)
         return (-1);
+    flen = field_len(line, sep, field_index);
+   
+  
+
+
     
     
     
