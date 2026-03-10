6be7b417365417d7f43aa502c668d02529cf33e9
Paola P <dppuertol@gmail.com>
2026-03-10 21:50:09 +0000

    add a test

---
+++ 03-tests/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *csv_get_field(const char *line, char sep, int field_index);
+int main(void)
+{   
+    char line[] = "Paris, Bogota, New York";
+    char *field;
+
+    field = csv_get_field(line, ',', 1);
+    if (!field)
+        return (1);
+    printf("%s\n", field);
+
+    free(field);
+    return (0);
+}
