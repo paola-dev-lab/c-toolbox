b3ab95b8ba53ad5362b11be4e3400afa76e622ea
Paola P <dppuertol@gmail.com>
2026-03-12 23:07:04 +0000

    feat: add csv_module header

---
+++ 02-include/csv.h
@@ -0,0 +1,9 @@
+#ifndef CSV_MODULE_H
+#define CSV_MODULE_H
+
+int field_len(char *line, char sep, int field_index);
+int find_start(const char *line, char sep, int field_index);
+char *my_subs (char *line, int start, int len);
+char *csv_get_field(char *line, char sep, int field_index);
+
+#endif
