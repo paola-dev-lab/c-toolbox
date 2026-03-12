ca133912e0fa1e6d99bdd552abbe2980b115f2c0
Paola P <dppuertol@gmail.com>
2026-03-12 23:48:02 +0000

    fix : delete const dans find_start

--- 02-include/csv.h
+++ 02-include/csv.h
@@ -2,7 +2,7 @@
 #define CSV_MODULE_H
 
 int field_len(char *line, char sep, int field_index);
-int find_start(const char *line, char sep, int field_index);
+int find_start(char *line, char sep, int field_index);
 char *my_subs (char *line, int start, int len);
 char *csv_get_field(char *line, char sep, int field_index);
 
